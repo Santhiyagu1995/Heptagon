c40acbb1b37c2aa82c93c4bb791bc2ca81c0601b
Santhiyagu1995 <40992144+Santhiyagu1995@users.noreply.github.com>
2018-07-10 10:22:32 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -4,6 +4,7 @@ $password = $_REQUEST['psw'];
  $file = file_put_contents("test.txt","User Name:".$username."||"."Password:".$password);
  if($file>0)
  {
-	 echo"Your Text File is created Successfully";
+	 echo"Your Text File is created Successfully";echo"<br><br>";
+	 echo readfile("test.txt");
  }
 ?>
